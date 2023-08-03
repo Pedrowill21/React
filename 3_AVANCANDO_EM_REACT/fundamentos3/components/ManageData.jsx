4b0bed7253ef118eebd947c49944b62a6a067dd2
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-08-03 17:31:04 -0300

    avançando em React

---
+++ 3_AVANCANDO_EM_REACT/fundamentos3/components/ManageData.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+import React from 'react'
+
+const ManageData = () => {
+
+  const SomeData = 10;
+
+  return (
+    <div>ManageData</div>
+  )
+}
+
+export default ManageData
---
+++ 3_AVANCANDO_EM_REACT/fundamentos3/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ManageData from '../components/ManageData'
+
+function App() {
+
+  return (
+    <div>
+      <img src="/jordan-andrews-tgDl3D1KPrQ-unsplash.jpg" alt="garoto"  style={{width:"400px"}}/>
+      <ManageData/>
+    </div>
+  )
+}
+
+export default App
