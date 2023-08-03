8abfa04969654938af5f41c2fff415d115dd26a2
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-08-02 21:47:49 -0300

    first commit

---
+++ 1_INTRO/hello/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="tituloPrincipal" >hello world React</h1>
+      <h2 className="subTitulo">Texto suuper criativo</h2>
+    </div>
+  );
+}
+
+export default App;
+ 
