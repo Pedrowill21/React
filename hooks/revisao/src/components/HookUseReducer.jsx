8faecbd039fae4b85ead236ad8c58d745818cde1
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-09-16 16:56:06 -0300

    hooks

---
+++ hooks/revisao/src/components/HookUseReducer.jsx
@@ -0,0 +1,16 @@
+import { useReducer } from "react"
+
+const HookUseReducer = () => {
+    const [number, dispatch] = useReducer((state, action) =>{
+        
+    })
+  return (
+    <div>
+        <h2>UseReducer</h2>
+        <hr />
+
+    </div>
+  )
+}
+
+export default HookUseReducer
